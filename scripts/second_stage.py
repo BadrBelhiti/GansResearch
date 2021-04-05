82f967a9ed88ceaefc8dcedc2d398352e37a8152
Badr Belhiti <s156956@live.killeenisd.org>
2021-04-05 18:28:30 -0500

    Tweaked conditional input architecture

--- scripts/second_stage.py
+++ scripts/second_stage.py
@@ -59,7 +59,7 @@ ngf = 64
 ndf = 32
 
 # Size of feature maps in conditional input layers
-ncf = 4
+ncf = 32
 
 # Number of training epochs
 num_epochs = 20
@@ -153,66 +153,63 @@ class Generator(nn.Module):
     def __init__(self, ngpu):
         super(Generator, self).__init__()
         self.ngpu = ngpu
+
+        # Convolutional layers before latent vector is concatenated
         self.conditional = nn.Sequential(
-            # input is (nc) x 128 x 128
             nn.Conv2d(nc, ncf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-             # state size. (ndf) x 64 x 64
+            
             nn.Conv2d(ncf, ncf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ncf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            
             nn.Conv2d(ncf * 2, ncf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ncf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+
             nn.Conv2d(ncf * 4, ncf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ncf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+
             nn.Conv2d(ncf * 8, ncf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ncf * 16),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(ncf * 16, ncf * 32, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ncf * 32),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(ncf * 32, ncf * 64, 4, 2, 1, bias=False),
             nn.ReLU(True)
         )
+
+        # Beginning of actual generator
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz + ncf * 64, ngf * 16, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz + 8192, ngf * 16, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.ReLU(True),
 
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*16) x 4 x 4
             nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
 
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*8) x 8 x 8
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # state size. (ngf*4) x 16 x 16
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf*2) x 32 x 32
             nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf) x 64 x 64
             nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # state size. (nc) x 128 x 128
         )
 
     def forward(self, input):
         noise, conditional_input = input
         conv_out = self.conditional(conditional_input)
+        conv_out = torch.flatten(conv_out, start_dim=1).reshape((128, -1, 1, 1))
+        print(conv_out.shape)
         return self.main(torch.cat((noise, conv_out), dim=1))
 
 # Create the generator
@@ -277,7 +274,7 @@ netD.apply(weights_init)
 
 # Initialize loss functions
 criterion = nn.BCELoss()
-conditional_criterion = nn.MSELoss()
+conditional_criterion = nn.L1Loss()
 
 # Create batch of latent vectors that we will use to visualize
 #  the progression of the generator
